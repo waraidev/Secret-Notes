7c36c696be18a009808ba13c673e9a430fe0542a
waraidev <wileye92@gmail.com>
2020-06-30 15:24:21 -0400

    Changed project name to Secret Notes
    Changed project name and undid some design changes

---
+++ SecretNotes/SecretNotes/ViewModels/NoteViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SecretNotes.Models;
+
+namespace SecretNotes.ViewModels
+{
+    public class NoteViewModel
+    {
+        
+    }
+}
